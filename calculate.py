b189f1bf55819ce6243e76d35c0bd5e03215c50a
Jelizaveta Lemeševa <jelizaveta.lemeseva@vinted.com>
2024-05-28 20:46:17 +0300

    Cleanup calculate.py

--- calculate.py
+++ calculate.py
@@ -3,21 +3,19 @@ from multiprocessing import Pool
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-# Connect to MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017")
-db = client["vessels"]
-collection = db["filtered"]
-
-# get all possible MMSI
-mmsis = collection.distinct('MMSI')[:10]
-
-print(mmsis)
-
-# Disconnect from MongoDB
-client.close()
-
 def process_mmsis(mmsis):
-  # Connect to MongoDB
+  '''
+  Function to process the MMSI data points and calculate the time difference between each data point
+
+  Parameters
+  ----------
+  :param mmsis: list of MMSI to process
+
+  Returns
+  -------
+  :return: list of time differences between each data point
+  '''
+  # connect to MongoDB
   client = pymongo.MongoClient("mongodb://localhost:27017")
   db = client["vessels"]
   collection = db["filtered"]
@@ -26,33 +24,44 @@ def process_mmsis(mmsis):
     data_points = collection.find({
       'MMSI': mmsi,
     }).sort( { '# Timestamp': 1 } )
-    print(f"Processing MMSI: {mmsi}")
+
     data_points = list(data_points)
     # calculate difference between each data points timestamp and save into a list
-    fmt = '%d/%m/%Y %H:%M:%S'
+    date_fmt = '%d/%m/%Y %H:%M:%S'
 
     for i in range(len(data_points) - 1):
-      tstamp1 = datetime.strptime(data_points[i+1]['# Timestamp'], fmt)
-      tstamp2 = datetime.strptime(data_points[i]['# Timestamp'], fmt)
+      tstamp1 = datetime.strptime(data_points[i+1]['# Timestamp'], date_fmt)
+      tstamp2 = datetime.strptime(data_points[i]['# Timestamp'], date_fmt)
       res.append(int(round((tstamp1 - tstamp2).total_seconds())))
 
-  # Disconnect from MongoDB
+  # disconnect from MongoDB
   client.close()
   return res
 
-num_processes = 4
-chunk_size = len(mmsis) // num_processes
-data_chunks = [mmsis[i:i+chunk_size] for i in range(0, len(mmsis), chunk_size)]
+if __name__ == '__main__':
+  # connect to MongoDB to get distinct MMSI
+  client = pymongo.MongoClient("mongodb://localhost:27017")
+  db = client["vessels"]
+  collection = db["filtered"]
 
-input("Press Enter to continue...")
-pool = Pool(processes=num_processes)
+  # get all distinct MMSI
+  mmsis = collection.distinct('MMSI')
 
-# Process the documents in parallel
-res = pool.map(process_mmsis, data_chunks)
-print(res)
-plt.hist([x for xs in res for x in xs], log=True)
-plt.show()
+  # disconnect from MongoDB for now
+  client.close()
 
-# Close the pool of worker processes
-pool.close()
-pool.join()
+  # process the MMSI data points in parallel
+  num_processes = 4 # do not use more than 4 processes as it will overload the database
+  chunk_size = len(mmsis) // num_processes
+  data_chunks = [mmsis[i:i+chunk_size] for i in range(0, len(mmsis), chunk_size)]
+
+  pool = Pool(processes=num_processes)
+  res = pool.map(process_mmsis, data_chunks)
+
+  # plot the histogram in log scale
+  plt.hist([x for xs in res for x in xs], log=True)
+  plt.show()
+
+  # close the pool of worker processes
+  pool.close()
+  pool.join()
